ef9f5ffd6b263c1585fa6ac32f85f9e64dfcf072
maestrow <anrew.shmelev@gmail.com>
2017-02-25 21:41:50 +0300

    Use FParsec.Pipes

--- md-fparsec/Parsing.Lists.fs
+++ md-fparsec/Parsing.Lists.fs
@@ -2,6 +2,7 @@
 
 open System
 open FParsec
+open FParsec.Pipes
 open MdFparsec.Ast
 open MdFparsec.Parsing.Tools
 open MdFparsec.Parsing.ParsingState
@@ -9,19 +10,20 @@ open MdFparsec.Parsing.ParsingState
 let (pList: Parser<Lists.List, _>), pListR = createParserForwardedToRef()
 
 // indentation
-let indent = pstring "  "
+let indent = %"  "
 let indentOfCurrentLevel = getUserState >>= (fun us -> parray us.ListLevel indent)
 
 // bullets
 let unorderedBullet = anyOf "*-+" |>> String.Concat 
-let orderedBullet = many1 digit .>> pchar '.' |>> (String.Concat >> (fun num -> num + "."))
-let bullet = unorderedBullet <|> orderedBullet .>> pchar ' ' .>> notFollowedBy spaces1
+let orderedBullet = many1 digit .>> %'.' |>> (String.Concat >> (fun num -> num + "."))
+let bullet = %[unorderedBullet; orderedBullet] .>> %' ' .>> notFollowedBy spaces1
 
 let textItem = 
-    indentOfCurrentLevel 
-    >>. bullet 
-    .>>. (many1 (noneOf "\r\n") |>> String.Concat)
-    .>> newline
+    %%    indentOfCurrentLevel 
+    -- +. bullet 
+    -- +. (many1 (noneOf "\r\n") |>> String.Concat)
+    --    newline
+    -%>   auto
 
 // list level
 let changeLevel delta = updateUserState (fun us -> { us with ListLevel = us.ListLevel + delta })
@@ -31,9 +33,9 @@ let levelDown = changeLevel -1
 let subList: Parser<_, State> = levelUp >>. pList .>> levelDown
 
 let item = 
-    textItem 
-    .>>. (attempt subList |>> Some <|> preturn None) 
-    |>> (fun ((bullet, text), sublist) -> Lists.ListItem (bullet, text, sublist))
+    %% +. textItem 
+    -- +. %[attempt subList |>> Some; preturn None]
+    -|>   (fun (bullet, text) sublist -> Lists.ListItem (bullet, text, sublist))
 
 pListR := many1 (attempt item) |>> (fun items -> Lists.CreateList items)
 
--- md-fparsec/Parsing.Spans.fs
+++ md-fparsec/Parsing.Spans.fs
@@ -2,6 +2,7 @@
 
 open System
 open FParsec
+open FParsec.Pipes
 open MdFparsec.Ast.Paragraph
 open MdFparsec.Parsing.Tools
 open MdFparsec.Parsing.ParsingState
@@ -14,10 +15,11 @@ let stackedSpanParserFactory = betweenWithStackCheck isNotInStack push pop
 
 let pText = many1 (noneOf "*_") |>> (String.Concat >> Text)
 
+
 let (spans: Parser<Span list, State>), spansR = createParserForwardedToRef()
 
-let pStrong = stackedSpanParserFactory StackedStrong (pchar '*') (pchar '*') spans |>> Strong
-let pEmphasis = stackedSpanParserFactory StackedEmphasis (pchar '_') (pchar '_') spans |>> Emphasis
-let pSpan = [pText; pStrong; pEmphasis] |> choice
+let pStrong = stackedSpanParserFactory StackedStrong %'*' %'*' spans |>> Strong
+let pEmphasis = stackedSpanParserFactory StackedEmphasis %'_' %'_' spans |>> Emphasis
+let pSpan = %[pText; pStrong; pEmphasis]
 
 spansR := many1 pSpan
